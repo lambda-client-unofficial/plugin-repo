fcfd631b9f3f084c9cb914259b2acb04809adbe6
ivy <ivy@not-ivy.dev>
2022-10-05 15:24:31 -0700

    check plugin version

--- check-version.sh
+++ check-version.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
 plugins=(plugins/*)
-for plugin in "${plugins[@]}"
-do
+for plugin in "${plugins[@]}"; do
     source=$(cat "$plugin/source")
     repo=${source#"https://github.com/"}
     repo=${repo%".git"}
     IFS=" " read -r -a version <<<"$(cat "$plugin/version")"
 
     remote_version="$(curl -s "https://raw.githubusercontent.com/$repo/master/src/main/resources/plugin_info.json" | jq .min_api_version | tr -d '"')"
+    releases="$(curl -s -H "Authorization: Bearer $API_TOKEN" "https://api.github.com/repos/$repo/releases" | jq .[0].tag_name | tr -d '"')"
 
+    plugin=${plugin#"plugins/"}
     if [ "$remote_version" = "${version[1]}" ]; then
-        echo "✔ $plugin"
+        echo "✔ $plugin (1/2)"
     else
-        echo "::error::$plugin has lambda api version ${version[1]} set, expected $remote_version"
+        echo "::warning::$plugin has lambda api version ${version[1]} set, expected $remote_version"
+    fi
+
+    if [ "$releases" = "null" ]; then
+        if [ "${version[0]}" = "HEAD" ]; then
+            echo "✔ $plugin (2/2)"
+        else 
+            echo "::warning::$plugin's version is set to ${version[0]} , expected HEAD"
+        fi
+    else
+        if [ "${version[0]}" = "$releases" ]; then
+            echo "✔ $plugin (2/2)"
+        else
+            echo "::warning::$plugin's version is set to ${version[0]}, expected $releases"
+        fi
     fi
 done
