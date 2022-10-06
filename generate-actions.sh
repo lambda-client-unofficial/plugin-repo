1ce99928a3abdcb53b580281af9da8100137456b
ivy <ivy@not-ivy.dev>
2022-10-05 19:53:35 -0700

    option to build HEAD

--- generate-actions.sh
+++ generate-actions.sh
@@ -2,8 +2,8 @@
 
 plugins="$(ls -m plugins)"
 
-cat << EOF > .github/workflows/gradle.yml
-name: Build and upload all plugins
+cat << EOF > .github/workflows/release.yml
+name: Build releases
 
 on: workflow_dispatch
 
@@ -24,6 +24,33 @@ jobs:
         distribution: 'temurin'
     - run: |
         ./build.sh \${{ matrix.plugins }}
+    - uses: actions/upload-artifact@v3.1.0
+      with:
+        path: build
+EOF
+
+cat << EOF > .github/workflows/latest.yml
+name: Build latest
+
+on: workflow_dispatch
+
+permissions:
+  contents: read
+
+jobs:
+  plugins_matrix:
+    strategy:
+      matrix:
+        plugins: [$plugins]
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v3
+    - uses: actions/setup-java@v3
+      with:
+        java-version: '8'
+        distribution: 'temurin'
+    - run: |
+        ./build.sh \${{ matrix.plugins }} --git
     - uses: actions/upload-artifact@v3.1.0
       with:
         path: build
